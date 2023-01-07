f6efcca5056d493030a27aaf0098413983846a76
EC2 Default User <ec2-user@ip-172-31-4-1.eu-central-1.compute.internal>
2023-01-07 13:24:25 +0000

    uploading project

---
+++ infrastructure/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  name          = "flask-final"
+  vpc_id        = "vpc-027b789fc39ef8f8c"
+  subnet_id     = "subnet-0c504d8d0af6c2a45"
+  instance_type = "t2.large"
+  my_ip         = "0.0.0.0/0"
+}
